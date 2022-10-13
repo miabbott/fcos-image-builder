65ee486ba296c8ae906651723e2cead1cb05b301
Micah Abbott <miabbott@redhat.com>
2022-10-13 09:09:34 -0400

    service: new custom reboot service
    Introduce a custom reboot service that fires after the completion of
    the ostree-finalize-staged.service. The service checks if there is a
    staged deployment and then reboots the system if present.

---
+++ usr/bin/post-upgrade-reboot.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+set -xeou pipefail
+
+# Check if the first deployment is booted; if false, should indicate that
+# an update was staged by `rpm-ostreed-automatic.service`
+if [ "$(rpm-ostree status --json | jq -r '.deployments[0]["booted"]')" == "true" ]; then
+    echo "First deployment is already booted; nothing to do."
+    exit 0
+fi
+
+echo "First deployment is not booted; upgrade (probably) staged; rebooting..."
+systemctl reboot
